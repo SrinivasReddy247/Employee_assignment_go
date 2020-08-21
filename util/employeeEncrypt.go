7781e5a04e93f89f314373d7d73d32be9ed5b04d
srinivasrdy247 <57893288+srinivasrdy247@users.noreply.github.com>
2020-08-21 18:06:14 +0530

    Add files via upload

---
+++ util/employeeEncrypt.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"crypto/rand"
+	"io"
+	"crypto/cipher"
+	"crypto/aes"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func Encrypt(data []byte, passphrase string) []byte {
+	block, err := aes.NewCipher([]byte(createHash(passphrase)))
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("nonce",string(nonce))
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	fmt.Println("ciper text:",string(ciphertext))
+	return ciphertext
+}
+
+func Decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error()) //a _b
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
